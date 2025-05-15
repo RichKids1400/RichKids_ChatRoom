8922dd95db5299e469e48ac5d13e8d1806cfbb07
nastaran sarkhosh <116547509+nastaranSarkhosh@users.noreply.github.com>
2025-05-15 08:55:48 +0330

    Create basic SignInScreen UI with email and password fields

--- app/src/main/java/com/g2/chatroom/feature/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/g2/chatroom/feature/auth/signin/SignInScreen.kt
@@ -1,1 +1,72 @@
+package com.g2.chatroom.feature.auth.signin
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.g2.chatroom.R
+
+@Composable
+fun SignInScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(it)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(200.dp)
+                    .background(Color.White)
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Email") }
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Button(
+                onClick = { /* TODO */ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Sign In")
+            }
+            TextButton(onClick = { navController.navigate("signup") }) {
+                Text(text = "Don't have an account? Sign Up")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSignInScreen() {
+    SignInScreen(navController = rememberNavController())
+}
